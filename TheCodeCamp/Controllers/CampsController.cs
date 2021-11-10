35009a45b0d5b86ceecaf2dc4947078935b3b470
cj.green <cj.green@intimetec.com>
2021-11-10 14:46:19 -0700

    added camp controller

---
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using TheCodeCamp.Data;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        private readonly ICampRepository _repository;
+
+        public CampsController(ICampRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<IHttpActionResult> Get()
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                //TODO Add logging
+                return InternalServerError(ex);
+            }
+            
+        }
+    }
+
+}
